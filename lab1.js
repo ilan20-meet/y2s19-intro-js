f498ad5b4030ebe93494e9bcd8b2f4a0bdb31291
myname21-meet <myname21@meet.mit.edu>
2019-07-23 11:31:26 -0400

    adaw

---
+++ lab1.js
@@ -0,0 +1,13 @@
+var color = prompt("What's your favourite colour?");
+if (colour == "purple")
+{
+	alert("very nice");
+}
+else if (colour == "green")
+{
+	alert("...");
+}
+else
+{
+	alert("ok");
+}
